f36ece35be29569dadf637e963f91e7d93118893
Isabelly Damascena <idamasce@thoughtworks.com>
2020-04-02 15:22:40 -0300

    Add logic to subtract lunch hour when user requires && extrac logic to create new date to a method

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ var vue = new Vue({
         firstEntry: "",
         secondEntry: "",
         workedHours: 0,
-        choosenTakeOffLunchHour: "yes",
+        chosenTakeOffLunchHour: "yes",
+        defaultLunchTime: 1
     },
     methods: {
         calculateHours: function (event) {
-            let initialDate = new Date(`2020/12/12 ${this.firstEntry}`);
-            let finalDate = new Date(`2020/12/12 ${this.secondEntry}`);
+            let initialDate = this._newDate(this.firstEntry);
+            let finalDate = this._newDate(this.secondEntry);
 
-            this.workedHours = this._roundToTwoDecimals(
+            let calculatedWorkedHours = this._roundToTwoDecimals(
                 (finalDate - initialDate) / kTransformMilissecondsToHours);
+
+            if (this._hasChosenToTakeLunchTimeOff()) {
+                calculatedWorkedHours -= this.defaultLunchTime;
+            }
+
+            this.workedHours = calculatedWorkedHours;
         }, 
         _roundToTwoDecimals: function(floatNumber){
             return floatNumber.toFixed(2);
+        },
+        _hasChosenToTakeLunchTimeOff: function() {
+            return this.chosenTakeOffLunchHour === "yes";
+        },
+        _newDate: function(hour) {
+            return new Date(`2020/12/12 ${hour}`);
         }
     }
 });
