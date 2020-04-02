afae263a7b9a59ac61f81b8d00604e3439cf45f5
Isabelly Damascena <idamasce@thoughtworks.com>
2020-04-02 14:49:15 -0300

    Round working hours to display 2 decimals

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ var vue = new Vue({
             let initialDate = new Date(`2020/12/12 ${this.firstEntry}`);
             let finalDate = new Date(`2020/12/12 ${this.secondEntry}`);
 
-            this.workedHours = (finalDate - initialDate) / kTransformMilissecondsToHours;
+            this.workedHours = this._roundToTwoDecimals(
+                (finalDate - initialDate) / kTransformMilissecondsToHours);
+        }, 
+        _roundToTwoDecimals: function(floatNumber){
+            return floatNumber.toFixed(2);
         }
     }
 });
