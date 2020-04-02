3044b1eb52bb097501c94c39c98e4c3ab1808232
Isabelly Damascena <idamasce@thoughtworks.com>
2020-04-02 14:01:52 -0300

    Calculate working hours with hour lunch time

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const kTransformMilissecondsToHours = (1000 * 60 * 60);
+
 var vue = new Vue({
     el: "#root",
     data: {
-        titleHeader: "Timesheet"
+        titleHeader: "Timesheet",
+        firstEntry: "",
+        secondEntry: "",
+        workedHours: 0
+    },
+    methods: {
+        calculateHours: function (event) {
+            let initialDate = new Date(`2020/12/12 ${this.firstEntry}`);
+            let finalDate = new Date(`2020/12/12 ${this.secondEntry}`);
+
+            this.workedHours = (finalDate - initialDate) / kTransformMilissecondsToHours;
+        }
     }
 });
